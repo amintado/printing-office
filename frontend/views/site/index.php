84360812ac6087dbe0d83f9d49f67b71c6530ed1
غلامرضا بهشتیان <amintado@gmail.com>
2017-09-05 11:33:18 +0430

    Change Backend Address

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,7 +1,19 @@
 <?php
+/*******************************************************************************
+ * Copyright (c) 2017.
+ * this file created in printing-office project
+ * framework: Yii2
+ * license: GPL V3 2017 - 2025
+ * Author:amintado@gmail.com
+ * Company:shahrmap.ir
+ * Official GitHub Page: https://github.com/amintado/printing-office
+ * All rights reserved.
+ ******************************************************************************/
 
 /* @var $this yii\web\View */
 
 $this->title =Yii::$app->name;
+
 ?>
 
+<img src="<?= Yii::$app->systemCore->downloadURL.'/web.jpg' ?>" class="img-responsive" style="height: 100vh;width: 100vw" alt="Image">
